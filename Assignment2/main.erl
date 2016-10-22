5edf0e9b571f683b297d2872d90fec34d164d71b
Robert Russo <rrusso44@gmail.com>
2016-10-22 19:05:29 -0400

    Input 1 Working
    Ayyyy

--- Assignment2/main.erl
+++ Assignment2/main.erl
@@ -1,9 +1,18 @@
 -module(main).
 -compile(export_all).
 
+invert(StartSeq, _, _) when StartSeq == [] -> [300, []];
+invert(StartSeq, TargetSeq, Characters) when hd(StartSeq) =:= TargetSeq -> io:format("LIST: ~p~n", [Characters]), [0, Characters];
+invert(StartSeq, TargetSeq, Characters) when hd(StartSeq) =/= TargetSeq ->
+  ReturnValue = invert(tl(StartSeq), TargetSeq, lists:append(Characters, hd(StartSeq))),
+  [1 + lists:nth(1, ReturnValue), lists:append(Characters, lists:nth(2, ReturnValue))].
+
 inversion_count(StartSeq, TargetSeq) when StartSeq == [] -> 0;
 inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =:= hd(TargetSeq) -> 0 + inversion_count(tl(StartSeq), tl(TargetSeq));
-inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =/= hd(TargetSeq) -> 1 + inversion_count(tl(StartSeq), tl(TargetSeq)).
+inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =/= hd(TargetSeq) ->
+  Values = invert(StartSeq, hd(TargetSeq), []),
+  io:format("DUMP ~p ~p ~p~n", [Values, tl(StartSeq), [lists:nth(2, Values)]]),
+  lists:nth(1, Values) + inversion_count([lists:nth(2, Values)], tl(TargetSeq)).
 
 do_work(StartSeq, TargetSeq, PID) ->
   io:format("PID: ~p, StartSeq: ~p, TargetSeq: ~p~n", [PID, StartSeq, TargetSeq]),
