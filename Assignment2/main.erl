b9713966e800ed8962abfa66f61787b68e3dd038
Robert Russo <rrusso44@gmail.com>
2016-10-22 17:03:05 -0400

    Updates Script & Printing

--- Assignment2/main.erl
+++ Assignment2/main.erl
@@ -1,7 +1,14 @@
 -module(main).
 -compile(export_all).
 
+inversion_count(StartSeq, TargetSeq) ->
+  if StartSeq =:= TargetSeq -> io:format("Works!");
+    true -> io:format("NAH~n")
+  end.
+
 do_work(StartSeq, TargetSeq, PID) ->
+  io:format("PID: ~p, StartSeq: ~p, TargetSeq: ~p~n", [PID, StartSeq, TargetSeq]),
+  inversion_count(StartSeq, TargetSeq),
   PID ! {ok, 4}.
 
 wait_for_done() ->
--- Assignment2/run.sh
+++ Assignment2/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+erl -compile main
 erl -pa ./main.erl -run main -run init stop -noshell
