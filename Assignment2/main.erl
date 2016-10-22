4ad156ee4ed24845016c7d9d7e514f5e87385cb0
Robert Russo <rrusso44@gmail.com>
2016-10-22 17:38:23 -0400

    Mismatch Count

--- Assignment2/main.erl
+++ Assignment2/main.erl
@@ -1,14 +1,14 @@
 -module(main).
 -compile(export_all).
 
-inversion_count(StartSeq, TargetSeq) ->
-  if StartSeq =:= TargetSeq -> io:format("Works!");
-    true -> io:format("NAH~n")
-  end.
+inversion_count(StartSeq, TargetSeq) when StartSeq == [] -> 0;
+inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =:= hd(TargetSeq) -> 0 + inversion_count(tl(StartSeq), tl(TargetSeq));
+inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =/= hd(TargetSeq) -> 1 + inversion_count(tl(StartSeq), tl(TargetSeq)).
 
 do_work(StartSeq, TargetSeq, PID) ->
   io:format("PID: ~p, StartSeq: ~p, TargetSeq: ~p~n", [PID, StartSeq, TargetSeq]),
-  inversion_count(StartSeq, TargetSeq),
+  Count = inversion_count(StartSeq, TargetSeq),
+  io:format("Mismatch Count: ~p~n", [Count]),
   PID ! {ok, 4}.
 
 wait_for_done() ->
