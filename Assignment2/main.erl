7d10a64c3e3d0e5d9a2b4d33b48c941130435c2e
Robert Russo <rrusso44@gmail.com>
2016-10-23 16:48:04 -0400

    Works on Input 2
    Input 3 is broken, but progress never the less

--- Assignment2/main.erl
+++ Assignment2/main.erl
@@ -2,8 +2,12 @@
 -compile(export_all).
 
 invert(StartSeq, _, _) when StartSeq == [] -> [300, []];
-invert(StartSeq, TargetSeq, Characters) when hd(StartSeq) =:= TargetSeq -> io:format("LIST: ~p~n", [Characters]), [0, Characters];
+invert(_, TargetSeq, _) when TargetSeq == [] -> [400, []];
+invert(StartSeq, TargetSeq, Characters) when hd(StartSeq) =:= TargetSeq ->
+  io:format("JOIN ELEMENTS: ~p + ~p~n", [Characters, StartSeq]),
+  [0, lists:append([Characters], tl(StartSeq))];
 invert(StartSeq, TargetSeq, Characters) when hd(StartSeq) =/= TargetSeq ->
+  io:format("COMBINING ~p + ~p~n", [Characters, hd(StartSeq)]),
   ReturnValue = invert(tl(StartSeq), TargetSeq, lists:append(Characters, hd(StartSeq))),
   [1 + lists:nth(1, ReturnValue), lists:append(Characters, lists:nth(2, ReturnValue))].
 
@@ -11,13 +15,13 @@ inversion_count(StartSeq, TargetSeq) when StartSeq == [] -> 0;
 inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =:= hd(TargetSeq) -> 0 + inversion_count(tl(StartSeq), tl(TargetSeq));
 inversion_count(StartSeq, TargetSeq) when hd(StartSeq) =/= hd(TargetSeq) ->
   Values = invert(StartSeq, hd(TargetSeq), []),
-  io:format("DUMP ~p ~p ~p~n", [Values, tl(StartSeq), [lists:nth(2, Values)]]),
-  lists:nth(1, Values) + inversion_count([lists:nth(2, Values)], tl(TargetSeq)).
+  io:format("RETURN VALUES ~p, EXISTING START SEQUENCE ~p, NEW START SEQUENCE ~p~n", [Values, tl(StartSeq), [lists:nth(2, Values)]]),
+  lists:nth(1, Values) + inversion_count(lists:nth(2, Values), tl(TargetSeq)).
 
 do_work(StartSeq, TargetSeq, PID) ->
   io:format("PID: ~p, StartSeq: ~p, TargetSeq: ~p~n", [PID, StartSeq, TargetSeq]),
   Count = inversion_count(StartSeq, TargetSeq),
-  io:format("Mismatch Count: ~p~n", [Count]),
+  io:format("Number of Inversions: ~p~n", [Count]),
   PID ! {ok, 4}.
 
 wait_for_done() ->
