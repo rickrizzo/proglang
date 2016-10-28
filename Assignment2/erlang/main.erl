1c72e829574404a2ba2445d93b657c0412d5c617
Robert Russo <rrusso44@gmail.com>
2016-10-27 20:06:43 -0400

    Concurrent

--- Assignment2/erlang/main.erl
+++ Assignment2/erlang/main.erl
@@ -1,51 +1,54 @@
 -module(main).
 -compile(export_all).
-findMatch(_, [], Count) -> 0;
+
+findMatch(_, [], _) -> 0;
 findMatch(MatchChar, [H|_], Count) when MatchChar =:= H -> Count + 1;
 findMatch(MatchChar, [H|T], Count) when MatchChar =/= H ->
   findMatch(MatchChar, T, Count + 1).
 
-merge(A, [], _) -> A;
-merge([], B, _) -> B;
-merge([HA|TA], [HB|TB], [HC|TC]) ->
+merge(A, [], _, _) -> A;
+merge([], B, _, _) -> B;
+merge([HA|TA], [HB|TB], [HC|TC], PID) ->
   if
     HA == HC ->
-      [HA | merge(TA, [HB|TB], TC)];
+      [HA | merge(TA, [HB|TB], TC, PID)];
     HB == HC ->
-      io:format("+1~n"),
-      [HB | merge([HA|TA], TB, TC)];
+      PID ! {sum, 1},
+      [HB | merge([HA|TA], TB, TC, PID)];
     true ->
       IndexA = findMatch(HC, [HA|TA], 0),
       IndexB = findMatch(HC, [HB|TB], 0),
       if
         IndexA > 0 ->
-          io:format("+~p~n", [IndexA]),
-          [HC | merge(lists:delete(HC, [HA|TA]), [HB|TB], TC)];
+          PID ! {sum, IndexA},
+          [HC | merge(lists:delete(HC, [HA|TA]), [HB|TB], TC, PID)];
         IndexB > 0 ->
-          io:format("+~p~n", [IndexB]),
-          [HC | merge([HA|TA], lists:delete(HC, [HB|TB]), TC)];
-        true-> io:format("NO MATCH! ~p ~p ~p~n", [HA, HB, HC]), lists:append([[HA|TA], [HB|TB]])
+          PID ! {sum, IndexB},
+          [HC | merge([HA|TA], lists:delete(HC, [HB|TB]), TC, PID)];
+        true->
+          lists:append([[HA|TA], [HB|TB]])
       end
   end.
 
-splitList([], _) -> [];
-splitList([E], _) -> [E];
-splitList(StartSeq, TargetSeq) ->
+splitList([], _, _) -> [];
+splitList([E], _, _) -> [E];
+splitList(StartSeq, TargetSeq, PID) ->
   {A, B} = lists:split(trunc(length(StartSeq) / 2), StartSeq),
   {TarA, TarB} = lists:split(trunc(length(TargetSeq) / 2), TargetSeq),
-  merge(splitList(A, TarA), splitList(B, TarB), TargetSeq).
+  merge(splitList(A, TarA, PID), splitList(B, TarB, PID), TargetSeq, PID).
 
 do_work(StartSeq, TargetSeq, PID) ->
-  io:format("~p~n", [splitList(StartSeq, TargetSeq)]),
-  PID ! {ok, 4}.
+  splitList(StartSeq, TargetSeq, PID),
+  PID ! {ok}.
 
-wait_for_done() ->
+wait_for_done(Sum) ->
   receive
-    {ok, Result} -> io:fwrite("~B~n", [Result])
+    {sum, A} -> wait_for_done(Sum + A);
+    {ok} -> io:fwrite("~B~n", [Sum])
   end.
 
 start() ->
   {ok, [StartSequence]} = io:fread("", "~s"),
   {ok, [TargetSequence]} = io:fread("", "~s"),
   spawn(main, do_work, [StartSequence, TargetSequence, self()]),
-  wait_for_done().
+  wait_for_done(0).
